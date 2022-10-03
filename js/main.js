423da74473641f1c6bdb79472c737e21a68df4bf
putn1k <skachok11@yandex.ru>
2022-10-03 16:09:47 +0300

    исправил аргумент в выводе функции(недоглядел)

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function getRandomPositiveNumber( min, max ) {
   const lower = Math.ceil( Math.min( min, max ) );
   const upper = Math.floor( Math.max( min, max ) );
 
-  return Math.floor( Math.random() * ( upper - lower + 1 ) + min );
+  return Math.floor( Math.random() * ( upper - lower + 1 ) + lower );
 }
 getRandomPositiveNumber( 5, 10 );
 
