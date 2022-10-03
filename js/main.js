ec43ab4ec339fb85562196d629eaf33529f41229
putn1k <skachok11@yandex.ru>
2022-10-03 15:25:54 +0300

    поправил название на более точное. упростил метод подсчета диапазона

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
-function getRandomNumber( min, max ) {
+function getRandomPosotiveNumber( min, max ) {
   if ( min < 0 || max < 0 || typeof( min ) !== 'number' || typeof( max ) !== 'number' ) {
     return NaN;
   }
-  if ( max < min ) {
-    min = Math.floor( min );
-    max = Math.ceil( max );
+  const lower = Math.ceil( Math.min( min, max ) );
+  const upper = Math.floor( Math.max( min, max ) );
 
-    return Math.floor( Math.random() * ( min - max + 1 ) + max );
-  }
-  min = Math.ceil( min );
-  max = Math.floor( max );
-
-  return Math.floor( Math.random() * ( max - min + 1 ) + min );
+  return Math.floor( Math.random() * ( upper - lower + 1 ) + min );
 }
-getRandomNumber( 5, 10 );
+getRandomPosotiveNumber( 5, 10 );
 
 function isLenghtOverflow( string, maxLength ) {
   return string.length <= maxLength;
