a58a13ba4c9429e6a3bc123ebccddc56198ae652
putn1k <skachok11@yandex.ru>
2022-10-04 17:18:40 +0300

    реализовал создание 25 случайных постов

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+let START_COUNTER = 0;
+const SIMILIAR_POSTS_COUNT = 25;
+const POST_DESCRIPTIONS = [
+  'Далеко-далеко за, словесными горами в стране гласных и согласных живут рыбные тексты. Его если путь родного вдали переписали но своих реторический снова, текста языком рыбного журчит языкового.',
+  'Которой рукопись то букв скатился всеми текстов пояс свой, заголовок она. Букв залетают текстами переулка моей большого курсивных не последний запятых переписали встретил снова океана всемогущая даль коварных.',
+  'Продолжил необходимыми ему текстами живет запятой текста инициал сбить. Толку, лучше скатился. Щеке до образ страна бросил строчка вершину реторический!',
+  'Его подзаголовок даль журчит реторический. Свое текстов путь большой пор над повстречался вершину, там решила! Гор все своего, рот сбить они образ.',
+  'Даль ручеек коварный точках реторический! Возвращайся домах решила имеет дал языком дорогу, не последний сих! Собрал до все что заголовок, строчка ведущими злых рукописи о, ручеек семантика, составитель коварных это?',
+  'Послушавшись, которой бросил она продолжил заголовок которое текстов использовало? Жаренные взобравшись продолжил точках необходимыми строчка, подпоясал,  своих сих щеке путь! Речью однажды грамматики запятой!'
+];
+const uniqueIDs = createUniqValues( SIMILIAR_POSTS_COUNT + 1 );
+const uniqueFileNames = createUniqValues( SIMILIAR_POSTS_COUNT + 1 );
+
 function getRandomPositiveNumber( min, max ) {
   if ( min < 0 || max < 0 ) {
     return NaN;
@@ -7,9 +20,43 @@ function getRandomPositiveNumber( min, max ) {
 
   return Math.floor( Math.random() * ( upper - lower + 1 ) + lower );
 }
-getRandomPositiveNumber( 5, 10 );
+
+function getLeadingZero( elementNumber ) {
+  return ( elementNumber.toString().length === 1 ) ? `0${elementNumber}` : elementNumber;
+}
+
+function getRandomArrayElement( elements ) {
+  return elements[ getRandomPositiveNumber( 0, elements.length - 1 ) ];
+}
 
 function isLengthOverflow( str, maxLength ) {
   return str.length <= maxLength;
 }
+
+function createUniqValues( count ) {
+  const uniqueValues = new Set();
+  while ( uniqueValues.size < count ) {
+    uniqueValues.add( getRandomPositiveNumber( 1, count ) );
+  }
+  return [ ...uniqueValues ];
+}
+
+function createPost() {
+  START_COUNTER++;
+  return {
+    id: uniqueIDs[ START_COUNTER ],
+    url: `photos/${getLeadingZero(uniqueFileNames[START_COUNTER])}.jpg`,
+    description: getRandomArrayElement( POST_DESCRIPTIONS ),
+    likes: getRandomPositiveNumber( 15, 200 ),
+    comments: getRandomPositiveNumber( 0, 200 ),
+  };
+}
+
+function createRandomPosts( count ) {
+  return Array.from( {
+    length: count
+  }, createPost );
+}
+
+createRandomPosts( SIMILIAR_POSTS_COUNT );
 isLengthOverflow( 'Hello, World', 12 );
