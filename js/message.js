f605d61756e06cb4dc30969d8c48cb59623d0c2f
putn1k <skachok11@yandex.ru>
2022-10-20 20:39:17 +0300

    поправил работоспособность функции. не работает, если написать слушатель одной строкой без фигурных скобок

--- js/message.js
+++ js/message.js
@@ -3,23 +3,27 @@ import {
 } from './utils.js';
 
 function isSuccsess() {
-  const successMessageTemplate = document.querySelector( '#success' ).content.querySelector( '.success' );
-  const successMessage = successMessageTemplate.cloneNode( true );
-  const closeSuccessMessageBtn = successMessage.querySelector( '.success__button' );
+  const successMessageTemplate = document.querySelector('#success').content.querySelector('.success');
+  const successMessage = successMessageTemplate.cloneNode(true);
+  const closeSuccessMessageBtn = successMessage.querySelector('.success__button');
 
-  document.body.append( successMessage );
+  document.body.append(successMessage);
   closeSuccessMessageBtn.focus();
-  closeSuccessMessageBtn.addEventListener( 'click', closeMessage( successMessage ) );
+  closeSuccessMessageBtn.addEventListener('click', () => {
+    closeMessage(successMessage);
+  });
 }
 
 function isError() {
-  const errorMessageTemplate = document.querySelector( '#error' ).content.querySelector( '.error' );
-  const errorMessage = errorMessageTemplate.cloneNode( true );
-  const closeErrorMessageBtn = errorMessage.querySelector( '.error__button' );
+  const errorMessageTemplate = document.querySelector('#error').content.querySelector('.error');
+  const errorMessage = errorMessageTemplate.cloneNode(true);
+  const closeErrorMessageBtn = errorMessage.querySelector('.error__button');
 
-  document.body.append( errorMessage );
+  document.body.append(errorMessage);
   closeErrorMessageBtn.focus();
-  closeErrorMessageBtn.addEventListener( 'click', closeMessage( errorMessage ) );
+  closeErrorMessageBtn.addEventListener('click', () => {
+    closeMessage(errorMessage);
+  });
 }
 
 export {
