1d902ffc4edd11587438d3bd78ffefacf2ae6d20
putn1k <skachok11@yandex.ru>
2022-10-31 22:08:41 +0300

    добавил модуль с константами

---
+++ js/constants.js
@@ -0,0 +1,15 @@
+const HttpMethod = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+const API_URL = 'https://27.javascript.pages.academy/kekstagram-simple';
+const JSON_HEADERS = {
+  'content-type': 'application/json'
+};
+
+export {
+  HttpMethod,
+  API_URL,
+  JSON_HEADERS,
+};
